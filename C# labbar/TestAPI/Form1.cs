bec6f48b32b605704c6fd7c431e92732772123e5
Lucas Granberg <33953582+luckyLucas4@users.noreply.github.com>
2019-04-30 09:47:18 +0200

    Added RoadData and TestAPI

---
+++ C# labbar/TestAPI/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        RoadConnect rc1 = new RoadConnect();
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            rc1.Start();
+        }
+    }
+}
